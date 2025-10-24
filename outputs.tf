7409ece2822b669bcd8a0b6d84a4723d30844360
hareesh3jampala <hareesh3jampala@gmail.com>
2025-10-24 18:17:47 +0530

    terraform

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "private_ip" {
     value = aws_instance.this.private_ip
     description = "Private IP of the instance created" 
 }
+
+output "instance_id" {
+    value = aws_instance.this.id
+  
+}
